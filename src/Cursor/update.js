5da591ad1f9a6cb99f29b27d1d06d8f5905f3701
Joseph Clay <joe@weareimmutable.com>
2020-07-08 16:30:34 -0500

    remove return values, change to object lookup for readability

--- src/Cursor/update.js
+++ src/Cursor/update.js
@@ -9,52 +9,58 @@ import {
   isPrimitive,
 } from '../types';
 
-const operations = new Map([
-  ['set', (target, key, value) => {
-    if (isObject(value)) return mergeDeep(target, { [key]: clone(value) });
-    return (target[key] = value);
-  }],
+const operations = {
+  set(target, key, value) {
+    if (isObject(value)) {
+      mergeDeep(target, { [key]: clone(value) });
+      return;
+    }
+    target[key] = value;
+  },
 
-  ['push', (target, key, value, path) => {
+  push(target, key, value, path) {
     if (!isArray(target[key])) throw new StoreError(`push`, { path });
-    return target[key].push(value);
-  }],
+    target[key].push(value);
+  },
 
-  ['unshift', (target, key, value, path) => {
+  unshift(target, key, value, path) {
     if (!isArray(target[key])) throw new StoreError(`unshift`, { path });
-    return target[key].unshift(value);
-  }],
+    target[key].unshift(value);
+  },
 
-  ['concat', (target, key, value, path) => {
+  concat(target, key, value, path) {
     if (!isArray(target[key])) throw new StoreError(`concat`, { path });
-    return target[key].push(...value);
-  }],
+    target[key].push(...value);
+  },
 
-  ['splice', (target, key, value, path) => {
+  splice(target, key, value, path) {
     if (!isArray(target[key])) throw new StoreError(`splice`, { path });
-    return target[key].splice(...value);
-  }],
+    target[key].splice(...value);
+  },
 
-  ['pop', (target, key, value, path) => {
+  pop(target, key, value, path) {
     if (!isArray(target[key])) throw new StoreError(`pop`, { path });  
-    return target[key].pop();
-  }],
+    target[key].pop();
+  },
 
-  ['shift', (target, key, value, path) => {
+  shift(target, key, value, path) {
     if (!isArray(target[key])) throw new StoreError(`shift`, { path });
-    return target[key].shift();
-  }],
+    target[key].shift();
+  },
 
-  ['unset', (target, key) => {
-    if (isArray(target)) return target.splice(key, 1);
+  unset(target, key) {
+    if (isArray(target)) {
+      target.splice(key, 1);
+      return;
+    }
     delete target[key];
-  }],
+  },
 
-  ['merge', (target, key, value, path) => {
+  merge(target, key, value, path) {
     if (!isObject(target[key])) throw new StoreError(`merge`, { path });
-    return mergeDeep(target[key], clone(value));
-  }],
-]);
+    mergeDeep(target[key], clone(value));
+  },
+};
 
 export default function update(data, path, type, value) {
   // dummy root, so we can shift and alter the root
@@ -65,9 +71,8 @@ export default function update(data, path, type, value) {
   let current = dummy;
   let key;
 
-  let idx = 0;
   const { length } = dummyPath;
-  for (; idx < length; idx++) {
+  for (let idx = 0; idx < length; idx++) {
     // Current item's reference is therefore p[s]
     // The reason why we don't create a variable here for convenience
     // is because we actually need to mutate the reference.
@@ -75,17 +80,14 @@ export default function update(data, path, type, value) {
 
     // if we reached the end of the path, we apply the operation
     if (idx === length - 1) {
-      return operations.get(type)(current, key, value, path);
+      return operations[type](current, key, value, path);
     }
     
     // if we reached a leaf, we override by setting an empty object
     if (isPrimitive(current[key])) {
       current[key] = {};
     }
-
+    
     current = current[key];
   }
-
-  // returning new data object
-  return current[key];
 };
