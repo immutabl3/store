70403ffc2b258be7d71381634272016dd9d8c212
Joseph Clay <joe@weareimmutable.com>
2020-07-08 14:16:19 -0500

    remove mapping

--- src/Transactions.js
+++ src/Transactions.js
@@ -1,18 +1,9 @@
-import { permute } from './utils';
-import query from './query';
-
 export default function Transactions() {
   const transactions = [];
   const map = new Map();
 
-  const getList = hash => {
-    if (map.has(hash)) return map.get(hash);
-    const list = [];
-    map.set(hash, list);
-    return list;
-  };
-
   return {
+    // TODO: just expose
     list() {
       return transactions;
     },
@@ -29,23 +20,6 @@ export default function Transactions() {
         path,
         value: args === undefined ? current : args,
       });
-    },
-    map() {
-      for (const transaction of transactions) {
-        const { path } = transaction;
-        const hash = query.hash(path);
-        const list = getList(hash);
-        list.push(transaction);
-
-        const permutations = permute(path);
-        for (const permutation of permutations) {
-          const hash = query.hash(permutation);
-          const list = getList(hash);
-          list.push(transaction);
-        }
-      }
-
-      return map;
     },
   };
 };
