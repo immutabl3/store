b94599a26f9e9eedc87a8f7e928cbba5e607d133
Joseph Clay <joe@weareimmutable.com>
2021-12-19 13:29:55 -0600

    update hook to avoid react 18 concurrency issues

--- src/react/hooks.js
+++ src/react/hooks.js
@@ -1,11 +1,9 @@
 import StoreError from '../StoreError.js';
 import Context from './context.js';
-import deepEqual from './fast-deep-equal';
 import React, {
   useContext as useReactContext,
   useState,
   useEffect,
-  useRef,
 } from 'react';
 import {
   isStore,
@@ -33,19 +31,17 @@ export const useContext = function(store, ctx = Context) {
 export const useStore = function(cursor, ctx = Context) {
   if (!isObjectLike(cursor) && !isFunction(cursor)) throw new StoreError(`invalid mapping`, { mapping: cursor });
 
-  const ref = useRef();
   const context = useReactContext(ctx);
-  const [state, setState] = useState(0);
-  ref.current = state;
-
   if (!context || !isStore(context.store)) throw new StoreError(`unable to locate store`, { context });
 
   const { store } = context;
   const mapping = isFunction(cursor) ? cursor(store.data) : cursor;
 
+  const [, setState] = useState(() => store.project(mapping));
+
   useEffect(() => {
-    return store.watch(mapping, () => setState(ref.current++));
+    return store.watch(mapping, ({ data }) => setState(data));
   }, mapping);
-    
+
   return store.project(mapping);
 };
