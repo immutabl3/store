40ac1d6adb0a4acb16c1c8bb772314d7897cf9b2
Joseph Clay <joe@weareimmutable.com>
2021-12-19 13:28:16 -0600

    remove unused dom enzyme initialization

--- test/fixtures.js
+++ test/fixtures.js
@@ -1,8 +1,5 @@
 import React from 'react';
 import Store from '../src/index.js';
-import once from 'lodash/once';
-import { configure } from 'enzyme';
-import ReactAdapter from 'enzyme-adapter-react-16';
 import Store from '../src';
 
 export const API = () => {
@@ -28,29 +25,6 @@ export const API = () => {
   return obj;
 };
 
-export const dom = once(() => {
-  configure({
-    adapter: new ReactAdapter(),
-  });
-
-  const { window } = new JSDOM('<!doctype html><html><body></body></html>');
-
-  global.window = window;
-  global.document = window.document;
-  global.navigator = {
-    userAgent: 'node.js',
-  };
-  global.requestAnimationFrame = function(callback) {
-    return setTimeout(callback, 0);
-  };
-  global.cancelAnimationFrame = function(id) {
-    clearTimeout(id);
-  };
-  Object.defineProperties(global, {
-    ...Object.getOwnPropertyDescriptors(window),
-    ...Object.getOwnPropertyDescriptors(global),
-  });
-});
 
 export const Component = ({
   value,
